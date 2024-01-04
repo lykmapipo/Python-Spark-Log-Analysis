97eaffa1a915503ea2dca02b9d5dce26e829c081
lykmapipo <lallyelias87@gmail.com>
2024-01-04 22:47:54 +0300

    feat(prepare): parse, format and normalize logs to a structured schema

---
+++ README.md
@@ -0,0 +1,53 @@
+# Python-Spark-Log-Analysis
+
+Python scripts to process, and analyze log files using PySpark.
+
+> 👋 This repository's maintainer is available to hire for Python/PySpark consulting projects. To get a cost estimate, send email to lallyelias87@gmail.com (for projects of any size or complexity).
+
+## Requirements
+
+- [Python 3.8+](https://www.python.org/)
+- [pip 23.3+](https://github.com/pypa/pip)
+- [pyarrow 14.0+](https://github.com/apache/arrow)
+- [pandas 2.0+](https://github.com/pandas-dev/pandas)
+- [pyspark 3.5+](https://github.com/apache/spark/tree/master/python)
+
+## Usage
+
+- Clone this repository
+```sh
+git clone https://github.com/lykmapipo/Python-Spark-Log-Analysis.git
+cd Python-Spark-Log-Analysis
+```
+
+- Install all dependencies
+
+```sh
+pip install -r requirements.txt
+```
+
+- To `parse and prepare` log files, run:
+```sh
+python prepare.py
+```
+
+## Contribute
+
+It will be nice, if you open an issue first so that we can know what is going on, then, fork this repo and push in your ideas. Do not forget to add a bit of test(s) of what value you adding.
+
+## Questions/Issues/Contacts
+
+lallyelias87@gmail.com, or open a GitHub issue
+
+
+## Licence
+
+The MIT License (MIT)
+
+Copyright (c) lykmapipo & Contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ prepare.py
@@ -0,0 +1,85 @@
+"""Prepare raw logs for analysis.
+
+This script parse, format and normalize raw logs into a structured format.
+
+It convert raw text log entry into structured logs schema below:
+root
+ |-- timestamp: timestamp (nullable = true)
+ |-- level: string (nullable = true)
+ |-- message: string (nullable = true)
+ |-- year: integer (nullable = true)
+ |-- month: integer (nullable = true)
+ |-- day: integer (nullable = true)
+ |-- hour: integer (nullable = true)
+ |-- minute: integer (nullable = true)
+ |-- second: integer (nullable = true)
+
+
+Command-Line Interface (CLI) Usage:
+    $ pip install pyspark[sql]
+    $ python prepare.py
+
+"""
+
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+from pyspark.sql import SparkSession
+
+SPARK_APP_NAME = "Python-Spark-Log-Analysis"
+SPARK_SHOW_NUM = 2
+
+# Start Spark session
+spark = SparkSession.builder.appName(SPARK_APP_NAME).getOrCreate()
+
+
+# Load raw logs data from text files into a DataFrame
+raw_logs_path = "data/raw/*.txt"
+df = spark.read.text(raw_logs_path).withColumnRenamed("value", "text")
+
+# Print raw logs dataframe schema
+print("\nRaw logs dataframe schema:")
+df.printSchema()
+
+# Print number of raw log entries
+print("\nRaw log entries number:", df.count())
+
+# Print top N raw log entries
+print(f"\nTop {SPARK_SHOW_NUM} raw log entries:")
+df.show(n=SPARK_SHOW_NUM, truncate=False)
+
+
+# Extract and cast log entry timestamp, level and message into their respective data types
+df = df.select(
+    F.regexp_extract(F.col("text"), r"\[(.*?)\]", 1).alias("timestamp").cast(T.TimestampType()),
+    F.regexp_extract(F.col("text"), r"\] (\w+):", 1).alias("level").cast(T.StringType()),
+    F.regexp_extract(F.col("text"), r": (.*)", 1).alias("message").cast(T.StringType()),
+)
+
+# Extract log entry year, month, dayofmonth, hour, minute and seconds from timestamp
+df = (
+    df.withColumn("year", F.year(F.col("timestamp")))
+    .withColumn("month", F.month(F.col("timestamp")))
+    .withColumn("day", F.dayofmonth(F.col("timestamp")))
+    .withColumn("hour", F.hour(F.col("timestamp")))
+    .withColumn("minute", F.minute(F.col("timestamp")))
+    .withColumn("second", F.second(F.col("timestamp")))
+)
+
+# Print stuctured logs dataframe schema
+print("\n\nStructured logs dataframe schema:")
+df.printSchema()
+
+# Print number of structure log entries
+print("\nStructured log entries number:", df.count())
+
+# Print top N structured log entries
+print(f"\nTop {SPARK_SHOW_NUM} stuctured log entries:")
+df.show(n=SPARK_SHOW_NUM, truncate=False)
+
+# Write structured logs dataframe
+structured_logs_path = "data/interim/structured"
+print(f"Write structured logs dataframe at {structured_logs_path}")
+df.write.mode("overwrite").parquet(structured_logs_path)
+
+# Stop Spark session
+spark.stop()
