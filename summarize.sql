abd9dee0592910fd5031bf8dc34aacfd0ef022fd
lykmapipo <lallyelias87@gmail.com>
2024-01-05 10:59:45 +0300

    feat(summarize): count log entries per year and per month

--- summarize.sql
+++ summarize.sql
@@ -1,16 +1,10 @@
 --
 -- Spark SQL query to aggregate and summarize structured logs.
 --
+
 SELECT
   'Total number of log entries' AS ANALYSIS,
   COUNT(*) AS VALUE
-FROM structured_logs
-
-UNION
-
-SELECT
-  'Average log entry length' AS ANALYSIS,
-  AVG(log_length) AS VALUE
 FROM structured_logs
 
 UNION
@@ -20,6 +14,29 @@ SELECT
   COUNT(*) AS VALUE
 FROM structured_logs
 GROUP BY log_level
+
+UNION
+
+SELECT
+  CONCAT('Total number of log entries in ', log_year) AS ANALYSIS,
+  COUNT(*) AS VALUE
+FROM structured_logs
+GROUP BY log_year
+
+UNION
+
+SELECT
+  CONCAT('Total number of log entries on ', log_month, ' month of the year') AS ANALYSIS,
+  COUNT(*) AS VALUE
+FROM structured_logs
+GROUP BY log_month
+
+UNION
+
+SELECT
+  'Average log entry length' AS ANALYSIS,
+  AVG(log_length) AS VALUE
+FROM structured_logs
 
 UNION
 
